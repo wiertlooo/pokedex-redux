f3442a0bedb4ab7ee58c47fd2f17c2eaba18477d
Wiktor <watulewicz@gmail.com>
2023-12-21 19:03:03 +0100

    style: search loop icon next to search bar

--- src/components/PokemonNav.tsx
+++ src/components/PokemonNav.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 function PokemonNav() {
   const navigate = useNavigate();
@@ -24,12 +25,22 @@ function PokemonNav() {
         </div>
       </div>
       <div className="flex h-20 items-center flex-end ml-auto mr-24">
-        <form onSubmit={(e) => searchSubmitHandle(e)}>
+        <form
+          onSubmit={(e) => searchSubmitHandle(e)}
+          className="flex flex-row items-center"
+        >
           <input
-            className="text-black"
+            placeholder="Enter pokemoon name/id"
+            className="text-black font-light"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
+          <button
+            type="submit"
+            className="text-black bg-white pt-1 pb-1 px-2 rounded-md ml-2"
+          >
+            <FaSearch />
+          </button>
         </form>
       </div>
     </div>
